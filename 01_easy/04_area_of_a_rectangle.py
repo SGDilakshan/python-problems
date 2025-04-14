1a87f0f38f9cf5901e549fa88a7c1b9ee15f4e29
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-14 13:00:18 +0530

    Update a Python program to calculate the area of a rectangle given its length and width.

---
+++ 01_easy/04_area_of_a_rectangle.py
@@ -0,0 +1,6 @@
+# Write a Python program to calculate the area of a rectangle given its length and width.
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+print(area)
