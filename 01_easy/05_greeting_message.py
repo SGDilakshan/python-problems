77a119b7e42890ea97dbb554282a07a879eb82b9
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-14 13:05:00 +0530

    Update a program that takes a user’s name and age as input and prints a greeting message.

---
+++ 01_easy/05_greeting_message.py
@@ -0,0 +1,6 @@
+# Create a program that takes a user’s name and age as input and prints a greeting message.
+
+name = input("Enter the name: ")
+age= input("Enter the age: ")
+print("Hello",name,"Now you are",age,"Years Old!")
+print(f"Hello {name} Now you are {age} Yaers Old!")
