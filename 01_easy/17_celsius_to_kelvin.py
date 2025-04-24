6dc9e2b8fbdc413e937718c77011172b8e589d92
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-24 13:01:58 +0530

    Create a program that takes a temperature in Celsius and converts it to Kelvin.

---
+++ 01_easy/17_celsius_to_kelvin.py
@@ -0,0 +1,5 @@
+# Create a program that takes a temperature in Celsius and converts it to Kelvin.
+
+celsius = float(input("Enter the temperature value in Celsius: "))
+kelvin = celsius + 273.15
+print(kelvin)
