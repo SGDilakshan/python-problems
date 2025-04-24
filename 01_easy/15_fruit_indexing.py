29273f109e37b5e063612853cba72773e7d872e8
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-24 12:50:15 +0530

    create a list of fruits and access elements using indexing updated

---
+++ 01_easy/15_fruit_indexing.py
@@ -0,0 +1,13 @@
+# Create a list of fruits and access elements using indexing.
+
+fruits = ["apple", "banana", "orange", "grape", "kiwi"]
+
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_fruit = fruits[-1]
+second_last_fruit = fruits[-2]
+
+print("First fruit:", first_fruit)
+print("Second fruit:", second_fruit)
+print("Last fruit:", last_fruit)
+print("Second last fruit:", second_last_fruit)
