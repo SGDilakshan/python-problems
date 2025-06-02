13d7861da757539586dadf24b77fc3003c7582c8
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 01:02:42 +0530

    Write a program that takes a sentence as input and counts the number of words in it updated.

---
+++ 02_intermediate/21_sentence.split.py
@@ -0,0 +1,5 @@
+# Write a program that takes a sentence as input and counts the number of words in it.
+
+sentence = input("Enter the sentence: ")
+words = sentence.split()
+print("Number of words:", len(words))
