eb1e8d9f4fdaa0a2b38f7d93325b40fae675e580
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 00:44:03 +0530

    Calculate the area of a triangle given its base and height using a function updated.

---
+++ 02_intermediate/18_triangle_area_calculator.py
@@ -0,0 +1,7 @@
+# Calculate the area of a triangle given its base and height using a function.
+
+def triangle_area(base, height):
+    return 0.5 * base * height
+
+print("Area of triangle:", triangle_area(10, 6))
+print("Area of triangle:", triangle_area(11, 5))
