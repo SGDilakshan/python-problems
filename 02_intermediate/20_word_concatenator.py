961cd26e4bd75ce37b4001e20d02881e3a4db93d
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 00:58:35 +0530

    Given a list of words, concatenate them into a single string separated by spaces updated.

---
+++ 02_intermediate/20_word_concatenator.py
@@ -0,0 +1,7 @@
+# Given a list of words, concatenate them into a single string separated by spaces.
+
+def concatenate_words(words):
+    return " ".join(words)
+
+words = ["Hello", "World", "Python", "Programming"]
+print(concatenate_words(words))
