e54be163e05f085e261f0f86560262d27a6f7fdd
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 00:46:28 +0530

    Create a function that takes a list of strings and returns the list sorted alphabetically updated.

---
+++ 03_hard/01_sort_strings_alphabetically.py
@@ -0,0 +1,7 @@
+# Create a function that takes a list of strings and returns the list sorted alphabetically.
+
+def sort_strings_alphabetically(string_list):
+    return sorted(string_list)
+
+strings = ["banana", "apple", "orange", "grape"]
+print("Sorted strings:", sort_strings_alphabetically(strings))
