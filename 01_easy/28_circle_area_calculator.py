aa3ba89cb35d124e71abd5b66f4a7f9bc333d595
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 00:13:08 +0530

    Write a function to calculate the area of a circle given its radius updated.

---
+++ 01_easy/28_circle_area_calculator.py
@@ -0,0 +1,8 @@
+# Write a function to calculate the area of a circle given its radius.
+
+import math
+
+def area_of_circle(radius):
+    return math.pi * radius * radius
+
+print("Area of circle:", area_of_circle(7))
