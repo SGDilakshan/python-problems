77f1ca77f233505ca3ffae08a0117a20168677ea
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-06 13:42:45 +0530

    Access elements in a tuple using indexing updated.

---
+++ 01_easy/30_tuple_index_accessing.py
@@ -0,0 +1,13 @@
+# Access elements in a tuple using indexing.
+
+tuple = ("Dilakshan", "cool", "dilli", "genuis")
+
+print("First element:", tuple[0])
+print("Second element:", tuple[1])
+print("Third element:", tuple[2])
+print("Fourth element:", tuple[3])
+print()
+print("First element:", tuple[-4])
+print("Second element:", tuple[-3])
+print("Third element:", tuple[-2])
+print("Fourth element:", tuple[-1])
