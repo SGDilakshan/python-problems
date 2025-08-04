a89ffdc92d72eb9a8ea5d74973948a5125540a85
SGDilakshan <sgd.dilakshan@gmail.com>
2025-08-04 23:49:29 +0530

    Reverse Words in a Sentence updated

--- 01_easy/45_reverse_words_in_sentence.py
+++ 01_easy/45_reverse_words_in_sentence.py
@@ -5,6 +5,5 @@ def reverse_words(sentence):
     reversed_words = words[::-1]
     return ' '.join(reversed_words)
 
-# Example usage
 sentence = input("Enter a sentence: ")
 print(reverse_words(sentence))
